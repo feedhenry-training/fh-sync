6627ef33172fc0ca739b6201db7ffd9d485426f7
John Frizelle <john.frizelle@feedhenry.com>
2013-02-06 12:45:50 +0000

    Refactored structure of JavaScript

--- client/default/js/admin.js
+++ client/default/js/admin.js
@@ -1,225 +1,288 @@
-/* JavaScript for managing administrator data collisions view */
-var collisionsTable;
-var collisionRecord;
-var activeCollision;
+var syncAdmin = (function() {
 
-var jsonEditorPre, jsonEditorPost, jsonEditorCurrent;
+  var self = {
 
-function getCollisions() {
-  sync.listCollisions(datasetId, showColissions, handleCollisionListFailure);
-}
+    /* JavaScript for managing administrator data collisions view */
+    collisionsTable: undefined,
+    collisionRecord: undefined,
+    activeCollision: undefined,
 
-function showColissions(res) {
-  // Store the collision data for use later
-  collisionRecord = res;
-  var tableData = [];
-  // Iterate over the dataset to create a record structure which is suitable for the jQuery Data table
-  // we are using to display the data (i.e a 2d array)
+    jsonEditorPre: undefined,
+    jsonEditorPost: undefined,
+    jsonEditorCurrent: undefined,
 
-  console.log(res);
-  
-  var controls = [];
-  controls.push('<button class="btn manage">Manage</button>&nbsp;');
-  controls.push('<button class="btn discard">Discard</button>&nbsp;');
-
-  for( i in res ) {
-    var row = [];
-    var rec = res[i];
-    row.push(i);
-    row.push(rec.uid);
-    row.push(rec.timestamp);
-    row.push(controls.join(""));
-    tableData.push(row);
-  }
-  reloadColissionsTable(tableData);
-
-}
-
-function reloadColissionsTable(contents) {
-  console.log('reloadTable :: ', contents);
-
-  $('#collisions_manager').show();
-  $('#collisions_editor').hide();
-  
-  if( contents.length == 0 ) {
-    $('#no_collisions').show();
-    $('#collisions_list').hide();
-    return;
-  }
-
-  // show the table & hide the no data message
-  $('#no_collisions').hide();
-  $('#collisions_list').show();
-
-  collisionsTable = $('#collisionsTable').dataTable( {
-    "bDestroy":true,
-    "aaData": contents,
-    "aoColumns": [
-      { "sTitle": "Collision Hash" },
-      { "sTitle": "Record UID" },
-      { "sTitle": "Collision Date" },
-      { "sTitle": "Controls", "bSortable": false, "sClass": "controls" }
-    ]
-  });
-
-  $('tr td .manage, tr td .discard, tr td:not(.controls,.dataTables_empty)').unbind().click(function() {
-    var row = $(this).parent().parent();
-    var data = collisionsTable.fnGetData($(this).closest('tr').get(0));
-
-    if($(this).hasClass('manage')) {
-      doManageCollision(data);
-    }
-    else if( $(this).hasClass('discard')) {
-      doDiscardCollision(data[0], false);
-    }
-    return false;
-  });
-}
-
-function handleCollisionListFailure(code, msg) {
-  alert('An error occured while listing collisions : (' + code + ') ' + msg);
-}
-
-function doManageCollision(data) {
-  var collisionHash = data[0];
-  var recordUid = data[1];
-  // Bind the discard button
-  $('.discard_collision_button').unbind().click(function() {
-    doDiscardCollision(collisionHash, true);
-  });
-
-  $('.save_collision_button').unbind().click(function() {
-    doSaveCollision(collisionHash, recordUid);
-  });
-
-  $('#back_button').unbind().click(function() {
-    console.log('back button click ', arguments);
-    $('#collisions_manager').show();
-    $('#collisions_editor').hide();
-    return false;
-  });
-
-  //Read the current version of the record
-  sync.read(datasetId, recordUid, function(record) {
-    var collisionRec = collisionRecord[collisionHash];
-    activeCollision = {
-      "pre" : collisionRec.pre,
-      "post" : collisionRec.post,
-      "current" : record.data
-    }
-
-    // Sort all the records to ensure their fields are listed in the same order for comparision
-    activeCollision.current = sortObject(activeCollision.current);
-    activeCollision.pre = sortObject(activeCollision.pre);
-    activeCollision.post = sortObject(activeCollision.post);
-
-    jsoneditorPre.jsonEditor(activeCollision.pre).find('input').attr('readonly', 'true');
-    jsoneditorPost.jsonEditor(activeCollision.post).find('input').attr('readonly', 'true');
-    jsoneditorCurrent.jsonEditor(activeCollision.current, {change: collisionChangeHandler}).find('input').attr('readonly', 'true');
-
-    $('.json-editor').find('input.property').addClass('span5').end()
-      .find('input.value').addClass('span7 pull-right');
-
-    // flat diff of json objects and highlight modified data,
-    // adding 'copy changes' button for each change
-
-    for (var key in activeCollision.current) {
-      // pre or post different?
-      console.log(key);
-      if ((activeCollision.pre && activeCollision.pre[key] !== activeCollision.current[key]) || (activeCollision.post && activeCollision.post[key] !== activeCollision.current[key])) {
-        if (activeCollision.pre){
-          var stringifiedKey = JSON.stringify(activeCollision.pre[key]);
-          var inputProperty = jsoneditorPre.find('input.property[title="' + key + '"]');
-          var inputRow = inputProperty.parent();
-          inputRow.addClass('activeCollision');
-          var copyPreBtn = $('<div>');
-          copyPreBtn.css({
-            "float": "right",
-            "width": 0,
-            "height": 0
-          });
-          copyPreBtn.append($('<button>', {
-            "class": "btn btn-inverse btnCopy btnCopyPre",
-            "text": ">"
-          }));
-          copyPreBtn.on('click', function (e) {
-            e.preventDefault();
-            var el = $(this);
-            var propName = el.prev().val();
-            jsoneditorCurrent.find('input.property[title="' + propName + '"]').next().val(el.next().val()).trigger('change');
-          });
-
-          inputProperty.after(copyPreBtn);
-        }
-
-        if (activeCollision.post){
-          jsoneditorPost.find('input.property[title="' + key + '"]').parent().addClass('activeCollision')
-            .find('input.property').before($('<div>').css({
-            "position": "absolute"
-          }).append($('<button>', {
-            "class": "btn btn-inverse btnCopy btnCopyPost",
-            "text": "<"
-          })).on('click', function (e) {
-              e.preventDefault();
-              var el = $(this);
-              var propName = el.next().val();
-              jsoneditorCurrent.find('input.property[title="' + propName + '"]').next().val(el.next().next().val()).trigger('change');
-            }));
-        }
-
-        jsoneditorCurrent.find('input.property[title="' + key + '"]').parent().addClass('activeCollision')
-          .find('input.value').removeAttr('readonly');
-      }
-    }
-
-    // Show the collision manager UI
-    $('#collisions_manager').hide();
-    $('#collisions_editor').show();
-  }, function(code, msg) {
-    alert('An error occured while reading the latest version of the record for collision management : (' + code + ') ' + msg);
-  });
-}
-
-function collisionChangeHandler() {
-  console.log('collisionChangeHandler : ', activeCollision.current);
-}
-
-function doDiscardCollision(collisionHash, hideEditor) {
-  sync.removeCollision(datasetId, collisionHash, function() {
-    //Colission successfully removed. Hide colission editor and reload table
-    if( hideEditor ) {
+    init: function() {
+      // Ensure the UI is set up correctly
+      $('#collisions_manager').show();
       $('#collisions_editor').hide();
+
+      // Map the jsonEdior variables to their corresponding HTML elements
+      self.jsoneditorCurrent = $('#jsoneditorCurrent');
+      self.jsoneditorPre = $('#jsoneditorPre');
+      self.jsoneditorPost = $('#jsoneditorPost');
+
+      // Bind UI buttons
+      $('#btnReloadCollisions').unbind().click(self.getCollisions);
+
+      $('#back_button').unbind().click(function() {
+        console.log('back button click ', arguments);
+        $('#collisions_manager').show();
+        $('#collisions_editor').hide();
+        return false;
+      });
+
+      self.getCollisions();
+    },
+
+
+    reloadCollisions: function() {
+      if( $('#collisions_manager').is(':visible ') ) {
+        self.getCollisions();
+      }
+    },
+
+    getCollisions: function() {
+      sync.listCollisions(datasetId, self.showColissions, self.handleCollisionListFailure);
+    },
+
+    showColissions: function(res) {
+      console.log('showCollisions : ', res);
+
+      // Store the collision data for use later
+      self.collisionRecord = res;
+      var tableData = [];
+
+      // Iterate over the collisions to create a record structure which is suitable for the jQuery Data table
+      // we are using to display the data (i.e a 2d array)
+
+      var controls = [];
+      controls.push('<button class="btn manage">Manage</button>&nbsp;');
+      controls.push('<button class="btn discard">Discard</button>&nbsp;');
+
+      for( i in res ) {
+        var row = [];
+        var rec = res[i];
+        row.push(i);
+        row.push(rec.uid);
+        row.push(new Date(rec.timestamp));
+        row.push(controls.join(""));
+        tableData.push(row);
+      }
+      self.reloadColissionsTable(tableData);
+    },
+
+    reloadColissionsTable: function(contents) {
+      $('#collisions_manager').show();
+      $('#collisions_editor').hide();
+
+      if( contents.length == 0 ) {
+        $('#no_collisions').show();
+        $('#collisions_list').hide();
+        return;
+      }
+
+      // show the table & hide the no data message
+      $('#no_collisions').hide();
+      $('#collisions_list').show();
+
+      self.collisionsTable = $('#collisionsTable').dataTable( {
+        "bDestroy":true,
+        "aaData": contents,
+        "bLengthChange": false,
+        "bFilter": false,
+        "aoColumns": [
+          { "sTitle": "Collision Hash" },
+          { "sTitle": "Record UID" },
+          { "sTitle": "Collision Date" },
+          { "sTitle": "Controls", "bSortable": false, "sClass": "controls" }
+        ]
+      });
+
+      $('tr td .manage, tr td .discard, tr td:not(.controls,.dataTables_empty)').unbind().click(function() {
+        var row = $(this).parent().parent();
+        var data = self.collisionsTable.fnGetData($(this).closest('tr').get(0));
+
+        if($(this).hasClass('manage')) {
+          self.doManageCollision(data);
+        }
+        else if( $(this).hasClass('discard')) {
+          self.doDiscardCollision(data[0], false);
+        }
+        return false;
+      });
+    },
+
+    handleCollisionListFailure: function(code, msg) {
+      alert('An error occured while listing collisions : (' + code + ') ' + msg);
+    },
+
+    doManageCollision: function(data) {
+      var collisionHash = data[0];
+      var recordUid = data[1];
+      // Bind the discard button
+      $('.discard_collision_button').unbind().click(function() {
+        self.doDiscardCollision(collisionHash, true);
+      });
+
+      $('.save_collision_button').unbind().click(function() {
+        self.doSaveCollision(collisionHash, recordUid);
+      });
+
+      //Read the current version of the record
+      sync.read(datasetId, recordUid, function(record) {
+        var collisionRec = self.collisionRecord[collisionHash];
+        self.activeCollision = {
+          "pre" : collisionRec.pre,
+          "post" : collisionRec.post,
+          "current" : record.data
+        }
+
+        // Sort all the records to ensure their fields are listed in the same order for comparision
+        self.activeCollision.current = self.sortObject(self.activeCollision.current);
+        self.activeCollision.pre = self.sortObject(self.activeCollision.pre);
+        self.activeCollision.post = self.sortObject(self.activeCollision.post);
+
+        // Initialise the JSON editoris with the collision data
+        self.jsoneditorPre.jsonEditor(self.activeCollision.pre).find('input').attr('readonly', 'true');
+        self.jsoneditorPost.jsonEditor(self.activeCollision.post).find('input').attr('readonly', 'true');
+        self.jsoneditorCurrent.jsonEditor(self.activeCollision.current, {change: self.collisionChangeHandler}).find('input').attr('readonly', 'true');
+
+        self.initCollisionUI();
+
+        // Show the collision manager UI
+        $('#collisions_manager').hide();
+        $('#collisions_editor').show();
+      }, function(code, msg) {
+        alert('An error occured while reading the latest version of the record for collision management : (' + code + ') ' + msg);
+      });
+    },
+
+    initCollisionUI: function() {
+      // Set UI layout for collision lists
+      $('.json-editor').find('input.property').addClass('span5');
+      $('.json-editor').find('input.value').addClass('span7 pull-right');
+
+      // Function to invoke when copying collision changes from pre or post records
+      var copyClickHandler = function(e) {
+        e.preventDefault();
+        var el = $(this);
+
+        var propName = el.parent().find('input.property').attr('title');
+        var propVal = el.parent().find('input.value').val();
+
+        var target = self.jsoneditorCurrent.find('input.property[title="' + propName + '"]').parent();
+        target.find('input.value').val(propVal).trigger('change');
+      }
+
+      // highlight conflict data and add 'copy changes' button for each change
+      for (var key in self.activeCollision.current) {
+        // pre or post different?
+
+        if (
+          (self.activeCollision.pre && self.activeCollision.pre[key] !== self.activeCollision.current[key]) ||
+          (self.activeCollision.post && self.activeCollision.post[key] !== self.activeCollision.current[key])
+        ) {
+          if (self.activeCollision.pre){
+            // JSON Table stringifies all keys, so we need to do the same in order to find UI elements
+            var stringifiedKey = JSON.stringify(self.activeCollision.pre[key]);
+            var inputProperty = self.jsoneditorPre.find('input.property[title="' + key + '"]');
+            var inputRow = inputProperty.parent();
+
+            // Highlight the data as a collision
+            inputRow.addClass('collision');
+
+            // Create and initialise a button for copying the pre data as the conflict resolution
+            var copyPreBtn = $('<div>');
+            copyPreBtn.css({
+              "float": "right",
+              "width": 0,
+              "height": 0
+            });
+            copyPreBtn.append($('<button>', {
+              "class": "btn btn-inverse btnCopy btnCopyPre",
+              "text": ">"
+            }));
+            copyPreBtn.on('click', copyClickHandler);
+
+            // Add the copy button to the UI
+            inputProperty.after(copyPreBtn);
+          }
+
+          if (self.activeCollision.post){
+            var stringifiedKey = JSON.stringify(self.activeCollision.post[key]);
+            var inputProperty = self.jsoneditorPost.find('input.property[title="' + key + '"]');
+            var inputRow = inputProperty.parent();
+
+            // Highlight the data as a collision
+            inputRow.addClass('collision');
+
+            // Create and initialise a button for copying the post data as the conflict resolution
+            var copyPostBtn = $('<div>');
+            copyPostBtn.css({
+              "position": "absolute"
+            });
+            copyPostBtn.append($('<button>', {
+              "class": "btn btn-inverse btnCopy btnCopyPost",
+              "text": "<"
+            }));
+            copyPostBtn.on('click', copyClickHandler);
+
+            inputProperty.before(copyPostBtn);
+          }
+
+          // Highlight the row in current version of the conflict record
+          self.jsoneditorCurrent.find('input.property[title="' + key + '"]').parent().addClass('collision')
+        }
+      }
+    },
+
+    collisionChangeHandler: function() {
+      console.log('collisionChangeHandler : ', self.activeCollision.current);
+    },
+
+    doDiscardCollision: function(collisionHash, hideEditor) {
+      sync.removeCollision(datasetId, collisionHash, function() {
+        //Colission successfully removed. Hide colission editor and reload table
+        if( hideEditor ) {
+          $('#collisions_editor').hide();
+        }
+        self.getCollisions();
+      }, function(code, msg) {
+        alert('An error occured while removing the collision record : (' + code + ') ' + msg);
+      });
+    },
+
+    doSaveCollision: function(collisionHash, recordUid) {
+      console.log('currentRec = ', self.activeCollision.current);
+      sync.update(datasetId, recordUid, self.activeCollision.current, function(res) {
+        alert('Collision successfully resolved');
+        self.doDiscardCollision(collisionHash, true);
+      }, function(code, msg) {
+        alert('An error occured while udating the collision record : (' + code + ') ' + msg);
+      });
+    },
+
+    sortObject: function(o) {
+      var sorted = {},
+        key, a = [];
+
+      for (key in o) {
+        if (o.hasOwnProperty(key)) {
+          a.push(key);
+        }
+      }
+
+      a.sort();
+
+      for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+      }
+      return sorted;
     }
-    getCollisions();
-  }, function(code, msg) {
-    alert('An error occured while removing the collision record : (' + code + ') ' + msg);
-  });
-}
+  };
 
-function doSaveCollision(collisionHash, recordUid) {
-  console.log('currentRec = ', activeCollision.current);
-  sync.update(datasetId, recordUid, activeCollision.current, function(res) {
-    alert('Collision successfully resolved');
-    doDiscardCollision(collisionHash, true);
-  }, function(code, msg) {
-    alert('An error occured while udating the collision record : (' + code + ') ' + msg);
-  });
-}
-
-function sortObject(o) {
-  var sorted = {},
-    key, a = [];
-
-  for (key in o) {
-    if (o.hasOwnProperty(key)) {
-      a.push(key);
-    }
+  return {
+    init: self.init,
+    reloadCollisions: self.reloadCollisions
   }
-
-  a.sort();
-
-  for (key = 0; key < a.length; key++) {
-    sorted[a[key]] = o[a[key]];
-  }
-  return sorted;
-}
+})();
---
+++ client/default/js/user.js
@@ -0,0 +1,212 @@
+var syncUser = (function() {
+
+  var self = {
+
+    syncTable: undefined,
+
+    init: function() {
+      // Ensure UI is set up correctly
+      $('#updateBtn').attr('disabled', 'disabled');
+
+      $('#isOnlineChk').unbind().click(self.setOnline);
+      $('#updateBtn').unbind().click(self.updateItem);
+      $('#addBtn').unbind().click(self.addItem);
+      $('#clearNotificationsBtn').unbind().click(self.clearNotifications);
+
+      // Initialise the Sync Service. See XXXX for details on initialisation options
+      sync.init({
+        "sync_frequency": 5,
+        "auto_sync_local_updates": true,
+        "notify_client_storage_failed": true,
+        "notify_sync_started": true,
+        "notify_sync_complete": true,
+        "notify_offline_update": true,
+        "notify_collision_detected": true,
+        "notify_update_failed": true,
+        "notify_update_applied": true,
+        "notify_delta_received": true
+      });
+
+      // Provide handler function for receiving notifications from sync service - e.g. data changed
+      sync.notify(self.handleSyncNotifications);
+
+      // Get the Sync service to manage the dataset called "myShoppingList"
+      sync.manage(datasetId, {});
+
+      // Request the initial dataset from the sync service
+      sync.list(datasetId, self.handleListSuccess, self.handleListFailure);
+    },
+
+    handleSyncNotifications: function(notification) {
+      var msg = new Date() + ' : ' + notification.code + ' (uid:' + notification.uid + ', msg:' + notification.message + ')\n';
+      $('#notifications').val(msg + $('#notifications').val());
+
+      if( 'sync_complete' == notification.code ) {
+        // We are interetsed in sync_complete notifications as there may be changes to the dataset
+        if( datasetHash != notification.uid ) {
+          // The dataset hash received in the uid parameter is different to the one we have stored.
+          // This means that there has been a change in the dataset, so we should invoke the list operation.
+          datasetHash = notification.uid;
+          sync.list(datasetId, self.handleListSuccess, self.handleListFailure);
+        }
+      }
+    },
+
+    clearNotifications: function() {
+      $('#notifications').val('');
+    },
+
+    handleListSuccess: function(res) {
+      var tableData = [];
+      // Iterate over the dataset to create a record structure which is suitable for the jQuery Data table
+      // we are using to display the data (i.e a 2d array)
+
+      var controls = [];
+      controls.push('<button class="btn edit">Edit</button>&nbsp;');
+      controls.push('<button class="btn delete">Delete</button>&nbsp;');
+
+      for( i in res ) {
+        var row = [];
+        var rec = res[i];
+        row.push(i);
+        row.push(rec.data.name);
+        row.push(new Date(rec.data.created));
+        row.push(controls.join(""));
+        tableData.push(row);
+      }
+
+      self.reloadTable(tableData);
+    },
+
+    handleListFailure: function(code, msg) {
+      alert('An error occured while listing data : (' + code + ') ' + msg);
+    },
+
+    addItem: function() {
+      console.log('addItem Called...');
+      var name = $('#itemIn').val();
+      var created = new Date().getTime();
+      var dataItem = {
+        "name" : name,
+        "created" : created
+      };
+      sync.create(datasetId, dataItem, function(res) {
+        console.log('Create item success');
+      }, function(code, msg) {
+        alert('An error occured while creating data : (' + code + ') ' + msg);
+      });
+
+      // Clear the add item text box
+      $('#itemIn').val('');
+    },
+
+    updateItem: function() {
+      // Read the data from the fields
+      var name = $('#itemUp').val();
+      var uid = $('#itemUpId').val();
+
+      // Reset fields and disable update button
+      $('#itemUp').val('');
+      $('#itemUpId').val('');
+      $('#updateBtn').attr('disabled', 'disabled');
+
+      // Read the full record from the sync service
+      sync.read(datasetId, uid, function(res) {
+        var data = res.data;
+        // Update the name field with the updated value from the text box
+        data.name = name;
+
+        // Send the update to the sync service
+        sync.update(datasetId, uid, data, function(res) {
+          console.log('Update item success');
+        },
+        function(code, msg) {
+          alert('Unable to update row : (' + code + ') ' + msg);
+        });
+      }, function(code, msg) {
+        alert('Unable to read row for updating : (' + code + ') ' + msg);
+      });
+    },
+
+    reloadTable: function(contents) {
+      console.log('reloadTable :: ', contents);
+      if( contents.length == 0 ) {
+        $('#nosyncdata').show();
+        $('#table').hide();
+        return;
+      }
+
+      // show the table & hide the no data message
+      $('#nosyncdata').hide();
+      $('#table').show();
+
+      self.syncTable = $('#shoppingList').dataTable( {
+        "bDestroy":true,
+        "bLengthChange": false,
+        "bFilter": false,
+        "aaData": contents,
+        "aoColumns": [
+          { "sTitle": "UID" },
+          { "sTitle": "Food Item" },
+          { "sTitle": "Date Created" },
+          { "sTitle": "Controls", "bSortable": false, "sClass": "controls" }
+        ]
+      });
+
+      $('tr td .edit, tr td .delete, tr td:not(.controls,.dataTables_empty)').unbind().click(function() {
+        var row = $(this).parent().parent();
+        var data = self.syncTable.fnGetData($(this).closest('tr').get(0));
+
+        if($(this).hasClass('edit')) {
+          self.doEditRow(data);
+        }
+        else if( $(this).hasClass('delete')) {
+          self.doDeleteRow(data);
+        }
+        return false;
+      });
+    },
+
+    doEditRow: function(row) {
+      sync.read(datasetId, row[0], function(res) {
+        console.log('read ', res);
+        $('#itemUp').val(res.data.name);
+        $('#itemUpId').val(row[0]);
+        $('#updateBtn').removeAttr('disabled');
+      },
+      function(code, msg) {
+        alert('Unable to read row for editing : (' + code + ') ' + msg);
+      });
+    },
+
+    doDeleteRow: function(row) {
+      sync.read(datasetId, row[0], function(res) {
+        var doDelete = confirm('Are you sure you wish to delete this row')
+        if( doDelete ) {
+          sync.delete(datasetId, row[0], function(res) {
+            console.log('Delete item success');
+          },
+          function(code, msg) {
+            alert('Unable to delete row : (' + code + ') ' + msg);
+          });
+        }
+      },
+      function(code, msg) {
+        alert('Unable to read row for deleting : (' + code + ') ' + msg);
+      });
+    },
+
+    setOnline: function() {
+      var isOnline = $('#isOnlineChk').is(":checked")
+      console.log('isOnline = ' + isOnline);
+      //navigator.network.connection.type
+      navigator.network = navigator.network || {};
+      navigator.network.connection = navigator.network.connection || {}
+      navigator.network.connection.type = isOnline ? 'WiFi' : 'none';
+    }
+  };
+
+  return {
+    init: self.init
+  }
+})();
